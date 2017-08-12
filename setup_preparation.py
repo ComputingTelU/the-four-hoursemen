1603a3f719212db3b27f6b2c3962a6dc017fb5a8
Satria H R Harsono <hafizhharsono@gmail.com>
2017-08-12 17:55:26 +0700

    Elaborating task to complete

---
+++ setup_preparation.py
@@ -0,0 +1,16 @@
+# SETUP PREPARATION
+# Setup Preparation for future simulation
+#
+
+# Primitive Polynom function
+
+
+# Generate C function
+
+
+# Generate V function
+
+
+# Generate Q function
+
+
