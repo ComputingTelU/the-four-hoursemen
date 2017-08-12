3e303c7bc0ad090b2091ec4f599bad72d15146b4
Ridhwan D <ridhwan.dewoprabowo@gmail.com>
2017-08-12 19:15:46 +0700

    Add generate_v function.

--- setup_preparation.py
+++ setup_preparation.py
@@ -9,7 +9,14 @@
 
 
 # Generate V function
+import random, sys
+from numpy import matrix
 
+def generate_v(n, gf):
+	v = [0 for i in range(n)]
+	while (v == [0 for i in range(n)]):
+		v = [random.randrange(sys.maxint)%gf for i in range(n)]
+	return v
 
 # Generate Q function
 
